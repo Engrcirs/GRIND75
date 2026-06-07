57d1b0bc95d3aa7d825adc0354823d8f0cb2a4ac
Cirs Inso <cirsjoyinso@outlook.com>
2026-06-07 23:17:34 +0800

    Valid Parenthesis Stacks

---
+++ Python/ValidParenthesis.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        
+        stack = []
+        #{key : value, c : value}
+        hashmap = {")" : "(", "}" : "{", "]" : "["} 
+        for c in s:
+            if c not in hashmap:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                else:
+                    popped = stack.pop()
+                    if popped != hashmap[c]:
+                        return False
+        return not stack
+    
